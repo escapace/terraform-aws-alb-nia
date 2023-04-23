6cc77505a6ea8ba8d6db6f21c09881b48ec09291
Mark Martirosian <hello@markmartirosian.com>
2023-04-23 18:26:08 +0000

    fix: updates

--- versions.tf
+++ versions.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.34.0"
+      version = "~> 4.64.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.2.0"
+      version = "~> 3.5.1"
     }
   }
 }
